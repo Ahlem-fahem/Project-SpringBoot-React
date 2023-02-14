ea7b3d222e8b8fddadf5586e50749323e5471dc2
Ahlem FAHEM <ahlem.fahem@sii.fr>
2023-02-14 16:02:14 +0100

    First commit

---
+++ src/main/java/com/example/myproject_java_springboot_react/MyProjectJavaSpringBootReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.myproject_java_springboot_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectJavaSpringBootReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyProjectJavaSpringBootReactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myproject_java_springboot_react/MyProjectJavaSpringBootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myproject_java_springboot_react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectJavaSpringBootReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
